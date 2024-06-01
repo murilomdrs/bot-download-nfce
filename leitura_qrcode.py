b040d8e2f439e75085f32c77e76dd944ea27ea98
murilomdrs <murilomdrs@gmail.com>
2024-06-01 12:02:16 -0300

    tratativa para extrair chave de acesso

--- leitura_qrcode.py
+++ leitura_qrcode.py
@@ -1,9 +1,10 @@
+# Importando bibliotecas
 from qreader import QReader
-# import cv2
+import cv2
 
 
 # URL's
-caminhoPastaProcessar_IN = "/Users/murilomedeiros/Documents/Processar/"
+caminhoPastaProcessar_IN = "/Users/murilomedeiros/Library/CloudStorage/OneDrive-Pessoal/RPA/Ambiente de Testes/Download de Notas e Cupons Fiscais/Processar/"
 caminhoImagem_IN = ""
 
 
@@ -14,14 +15,19 @@ def main():
         qreader = QReader()
 
         # Get the image that contains the QR code
-        image = cv2.cvtColor(cv2.imread(caminhoPastaProcessar_IN + "image.jpeg"), cv2.COLOR_BGR2RGB)
-
-        print(image)
+        image = cv2.cvtColor(cv2.imread(caminhoPastaProcessar_IN + "image2.jpeg"), cv2.COLOR_BGR2RGB)
 
         # Use the detect_and_decode function to get the decoded QR data
         decoded_text = qreader.detect_and_decode(image=image)
 
-        print(decoded_text)
+        # Tratativa de limpar a string para capturar apenas a chave de acesso
+        decoded_text = decoded_text[0]
+        decoded_text = decoded_text.split("|")
+        decoded_text = decoded_text[0]
+        decoded_text = decoded_text.split("=")
+        decoded_text = decoded_text[1]
+        print(f"Chave de acesso extraída: {decoded_text}")
+
     
     except Exception as e:
         print(e)
